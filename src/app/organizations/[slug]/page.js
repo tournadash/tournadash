286746ee6033d33c274d806de08e94dcfb4fb5a1
SpyGamingOG <vaibhavvishwakarma0322@gmail.com>
2026-06-21 22:13:22 +0530

    Ui Update Fixes

--- src/app/organizations/[slug]/page.js
+++ src/app/organizations/[slug]/page.js
@@ -217,7 +217,7 @@ export default function OrgPublicProfilePage() {
           <div style={{
             position: 'absolute',
             top: 0, left: 0, right: 0, bottom: 0,
-            backgroundColor: 'rgba(0,0,0,0.65)',
+            backgroundColor: 'rgba(0,0,0,0.2)',
             zIndex: 1
           }} />
 
